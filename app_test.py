aee5d04cf9643fc4654cef315b38030895d70cfc
Ma Chue <ma.chue.mc@gmail.com>
2023-07-08 23:44:25 +0800

    Add more test cases

--- app_test.py
+++ app_test.py
@@ -1,41 +1,66 @@
 import os
+import shutil
 import unittest
 from flask import Flask
 from app import app, UploadAgent, ClassifierAgent, MoveFileAgent
-from werkzeug.datastructures import FileStorage
+from werkzeug.datastructures import FileStorage, FileMultiDict
+from flask.testing import FlaskClient
 
-# We use unittest.TestCase as the base class for our test case as it provides a rich set of methods for assertions and setup/teardown routines.
+# Extend FlaskClient for multipart file upload
+class FlaskClientWithFileUpload(FlaskClient):
+    def open(self, *args, **kwargs):
+        if 'data' in kwargs and isinstance(kwargs['data'], dict):
+            data = kwargs.pop('data')
+            kwargs['data'] = FileMultiDict(data)
+        return super().open(*args, **kwargs)
+
+# Replace test client class in Flask app
+app.test_client_class = FlaskClientWithFileUpload
+
 class AppTestCase(unittest.TestCase):
-
     def setUp(self):
-        # We use Flask's built-in test client for our tests. This allows us to simulate HTTP requests to our application without needing to run an actual server.
         self.app = app
-        self.client = self.app.test_client()
+        self.client = app.test_client()
+        app.config['TESTING'] = True
 
-    def test_upload_file(self):
-        # We test the home page as a simple smoke test to ensure the app is running correctly.
-        response = self.client.get('/')
+   # Test if the endpoint handles upload without any file
+    def test_upload_no_files(self):
+        response = self.client.post('/upload', data={}, follow_redirects=True)
         self.assertEqual(response.status_code, 200)
+        # Check for a part of the HTML that's always returned
+        self.assertIn(b'<!DOCTYPE html>', response.data)
 
-    def test_upload_files(self):
-        # We create the uploads directory if it doesn't exist. This is to ensure that the file upload functionality can be tested even if the directory was deleted.
-        if not os.path.exists('uploads'):
-            os.makedirs('uploads')
-        # We create a test file to simulate a user uploading a file.
-        with open('uploads/test_file.txt', 'w') as f:
+    # Test if the endpoint can upload a single file correctly
+    def test_upload_single_file(self):
+        with open('test_file.txt', 'w') as f:
             f.write('This is a test file.')
-        # We use Werkzeug's FileStorage class to simulate a file being uploaded.
-        with open('uploads/test_file.txt', 'rb') as f:
-            file = FileStorage(stream=f, filename='test_file.txt')
-            data = {
-                'file': file
-            }
-            # We send a POST request to the upload endpoint with the test file to test the file upload functionality.
-            response = self.client.post('/upload', data=data, content_type='multipart/form-data')
-        self.assertEqual(response.status_code, 302)
 
+        with open('test_file.txt', 'rb') as f:
+            data = {
+                'files[]': f
+            }
+            response = self.client.post('/upload', data=data, follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'test_file.txt', response.data)
+
+    # Test if the endpoint can handle multiple file upload correctly
+    def test_upload_multiple_files(self):
+        with open('test_file1.txt', 'w') as f:
+            f.write('This is a test file.')
+        with open('test_file2.txt', 'w') as f:
+            f.write('This is another test file.')
+
+        with open('test_file1.txt', 'rb') as f1, open('test_file2.txt', 'rb') as f2:
+            data = {
+                'files[]': [f1, f2]
+            }
+            response = self.client.post('/upload', data=data, follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'test_file1.txt', response.data)
+        self.assertIn(b'test_file2.txt', response.data)
+
+    # Test if the UploadAgent works as expected
     def test_UploadAgent(self):
-        # We test the UploadAgent function by creating a test file and checking that it is correctly moved to the safe or suspicious directory.
         if not os.path.exists('uploads'):
             os.makedirs('uploads')
         with open('uploads/test_file.txt', 'w') as f:
@@ -45,8 +70,8 @@ class AppTestCase(unittest.TestCase):
             UploadAgent(file)
         self.assertTrue(os.path.exists('safe/test_file.txt') or os.path.exists('suspicious/test_file.txt'))
 
+    # Test if the ClassifierAgent can classify files correctly
     def test_ClassifierAgent(self):
-        # We test the ClassifierAgent function by creating a test file and checking that it is correctly classified and moved to the safe or suspicious directory.
         if not os.path.exists('uploads'):
             os.makedirs('uploads')
         with open('uploads/test_file.txt', 'w') as f:
@@ -54,8 +79,8 @@ class AppTestCase(unittest.TestCase):
         ClassifierAgent('test_file.txt')
         self.assertTrue(os.path.exists('safe/test_file.txt') or os.path.exists('suspicious/test_file.txt'))
 
+    # Test if the MoveFileAgent can move files to the specified location correctly
     def test_MoveFileAgent(self):
-        # We test the MoveFileAgent function by creating a test file and checking that it is correctly moved to the specified directory.
         if not os.path.exists('uploads'):
             os.makedirs('uploads')
         with open('uploads/test_file.txt', 'w') as f:
@@ -63,20 +88,31 @@ class AppTestCase(unittest.TestCase):
         MoveFileAgent('test_file.txt', 'safe')
         self.assertTrue(os.path.exists('safe/test_file.txt'))
 
+    # Test if the endpoint for viewing files returns the correct response
     def test_view_files(self):
-        # We test the view files endpoint to ensure it is working correctly.
         response = self.client.get('/view')
         self.assertEqual(response.status_code, 200)
-    
+
+    # Clean up files and directories after each test
     def tearDown(self):
-        # We clean up the test files after each test to ensure that the file system is in a clean state for the next test.
+        if os.path.exists('test_file.txt'):
+            os.remove('test_file.txt')
+        if os.path.exists('test_file1.txt'):
+            os.remove('test_file1.txt')
+        if os.path.exists('test_file2.txt'):
+            os.remove('test_file2.txt')
         if os.path.exists('uploads/test_file.txt'):
             os.remove('uploads/test_file.txt')
         if os.path.exists('safe/test_file.txt'):
             os.remove('safe/test_file.txt')
         if os.path.exists('suspicious/test_file.txt'):
             os.remove('suspicious/test_file.txt')
+        if os.path.exists('safe'):
+            shutil.rmtree('safe')
+        if os.path.exists('suspicious'):
+            shutil.rmtree('suspicious')
+        if os.path.exists('uploads'):
+            shutil.rmtree('uploads')
 
-# We use the standard Python idiom for the main entry point of the script. This allows the script to be used both as a standalone program and as a module in other scripts.
 if __name__ == "__main__":
     unittest.main()
